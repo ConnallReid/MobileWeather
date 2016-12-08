1f531a52f8a799a0cf84c2765723ed0dabd50d8a
Connall <connall@thereidfamily.co.uk>
2016-12-08 15:59:34 +0000

    Favorites Layout Added

--- app/src/main/java/com/coursework/connall/mobileweather/AboutDialogue.java
+++ app/src/main/java/com/coursework/connall/mobileweather/AboutDialogue.java
@@ -16,12 +16,11 @@ public class AboutDialogue extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder mcAboutDialog = new AlertDialog.Builder(getActivity());
-        mcAboutDialog.setMessage(R.string.dialog_About)
-                .setPositiveButton(R.string.dialog_About_OK_btn, new
-                        DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                            }
-                        });
+        mcAboutDialog.setMessage(R.string.dialog_About).setPositiveButton(R.string.dialog_About_OK_btn, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
         mcAboutDialog.setTitle("About");
         //mcAboutDialog.setIcon(R.drawable.ic_about);
         // Create the AlertDialog object and return it
--- app/src/main/java/com/coursework/connall/mobileweather/FavoriteActivity.java
+++ app/src/main/java/com/coursework/connall/mobileweather/FavoriteActivity.java
@@ -2,25 +2,100 @@ package com.coursework.connall.mobileweather;
 
 
 import android.app.DialogFragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
 import com.google.android.gms.maps.SupportMapFragment;
+
+import java.util.LinkedList;
 
 /**
  * Created by Connall on 07/12/2016.
  */
 
 public class FavoriteActivity extends AppCompatActivity {
+    public LinkedList<WeatherRssItem> alist=new LinkedList<WeatherRssItem>();;
+    private ListView city;
+    WeatherDBinfo DbWeatherInfo;
+    LinkedList<WeatherRssItem> WeatherList=null;
+
+    FragmentManager fmAboutDialogue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.favorite_layout);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        android.support.v7.app.ActionBar ccActionBar = getSupportActionBar();
+        if (ccActionBar != null)
+        {
+            ccActionBar.setDisplayShowHomeEnabled(true);
+            //ccActionBar.setLogo(R.drawable.cloudy);
+            ccActionBar.setDisplayUseLogoEnabled(true);
+        }
+
+        fmAboutDialogue = this.getFragmentManager();
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.aberdeen:
+                ((TextView)findViewById(R.id.FavoriteLocation)).setText("Aberdeen");
+                return true;
+
+            case R.id.dundee:
+                ((TextView)findViewById(R.id.FavoriteLocation)).setText("Dundee");
+                return true;
+
+            case R.id.edinburgh:
+                ((TextView)findViewById(R.id.FavoriteLocation)).setText("Edinburgh");
+                return true;
+
+            case R.id.fortwillaim:
+                ((TextView)findViewById(R.id.FavoriteLocation)).setText("Fort Willaim");
+                return true;
+
+            case R.id.glasgow:
+                ((TextView)findViewById(R.id.FavoriteLocation)).setText("Glasgow");
+                return true;
+
+            case R.id.inverness:
+                ((TextView)findViewById(R.id.FavoriteLocation)).setText("Inverness");
+                return true;
+
+            case R.id.perth:
+                ((TextView)findViewById(R.id.FavoriteLocation)).setText("Perth");
+                return true;
+
+            case R.id.stirling:
+                ((TextView)findViewById(R.id.FavoriteLocation)).setText("Stirling");
+                return true;
+
+            case R.id.thurso:
+                ((TextView)findViewById(R.id.FavoriteLocation)).setText("Thurso");
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
